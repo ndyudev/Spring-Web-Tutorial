26e4d6f6bdadda28c9f3b7a01040e65b3510b404
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-04-25 12:44:07 +0700

    Init Project

---
+++ Spring-Tutorial-Web/src/main/java/com/spring/tutorial/Spring_Tutorial_Web/SpringTutorialWebApplication.java
@@ -0,0 +1,13 @@
+package com.spring.tutorial.Spring_Tutorial_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialWebApplication.class, args);
+	}
+
+}
---
+++ Spring-Tutorial-Web/src/test/java/com/spring/tutorial/Spring_Tutorial_Web/SpringTutorialWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.tutorial.Spring_Tutorial_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
